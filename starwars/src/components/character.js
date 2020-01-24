3e90c8c875673be07fd55aa943ba34de954785eb
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-01-24 13:13:04 -0500

    Built out cards, finished MVP

---
+++ starwars/src/components/character.js
@@ -0,0 +1,53 @@
+import React, {useState, useEffect} from 'react';
+import styled from "styled-components";
+import Axios from "axios";
+
+
+const Card = styled.div`
+    width: 300px;
+    border: 1px solid black;
+    border-radius: 10px;
+    margin: 25px 0;
+    min-height: 200px;
+    background-color: #443e3e41;
+`;
+
+const Name = styled.h3`
+    color: #443e3e;
+    text-shadow: 1px 1px 5px #fff;
+`;
+
+const Detail = styled.p`
+    color: #443e3e;
+    text-shadow: 1px 1px 5px #fff;
+`;
+
+export default function Character(props) {
+    const character = props.character;
+    const [homeworld, setHomeworld] = useState('');
+    
+
+    useEffect(() => {
+        if (homeworld === '') {
+        Axios.get(character.homeworld)
+            .then((response) =>{
+                setHomeworld(response.data.name)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+        }
+    }, [homeworld, setHomeworld])
+
+  
+
+    return (
+        <Card>
+            <Name>{character.name}</Name>
+            <Detail><strong>Height: </strong>{character.height}</Detail>
+            <Detail><strong>Mass: </strong>{character.mass}</Detail>
+            <Detail><strong>Birth year: </strong>{character.birth_year}</Detail>
+            {homeworld !== '' ? <Detail><strong>Homeworld: </strong>{homeworld}</Detail>:<></>}
+        </Card>
+    )
+}
--- starwars/src/components/characters.js
+++ starwars/src/components/characters.js
@@ -1,10 +1,22 @@
 import React, {useState, useEffect} from "react";
 import Axios from "axios";
 import styled from "styled-components";
+import Character from "./character";
 
+const Container = styled.div`
+    max-width: 1200px;
+    margin: 0 auto;
+    margin-bottom: 80px;
+    border: 1px solid black;
+    border-radius: 10px;
 
+    box-style: border-box;
+    padding: 50px;
 
-
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+`;
 
 export default function Characters() {
 
@@ -12,20 +24,25 @@ export default function Characters() {
 
   useEffect(() => {
     if (characters.length < 9) {
-    Axios.get(`https://swapi.co/api/people/${characters.length + 1}/`)
-      .then((response) => {
-        setCharacters([...characters, response])
-        console.log(characters)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+        Axios.get(`https://swapi.co/api/people/${characters.length + 1}/`)
+            .then((response) => {
+                setCharacters([...characters, response.data])
+                console.log(characters)
+            })
+            .catch((error) => {
+                console.log(error)
+            });
     }
+
   }, [characters, setCharacters]);
 
 
   return (
-      <></>
+      <Container>
+          {characters.map((item, index) => {
+              return <Character key={index} character={item}/>
+          })}
+      </Container>
   )
   
 }
