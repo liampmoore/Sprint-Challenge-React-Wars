d62e87320068a9d31ea1c3e8c96d22526c77b972
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-01-24 17:14:57 -0500

    Updates

---
+++ precache-manifest.b17044367ba3f574a6dcbce4fcb7991a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c630d2c1c6c344bc50f081de300ef348",
+    "url": "/Sprint-Challenge-React-Wars/index.html"
+  },
+  {
+    "revision": "51ee9ad5832da4ed41f8",
+    "url": "/Sprint-Challenge-React-Wars/static/css/main.9fb20e6a.chunk.css"
+  },
+  {
+    "revision": "eb0fab049939fd788c72",
+    "url": "/Sprint-Challenge-React-Wars/static/js/2.4139510e.chunk.js"
+  },
+  {
+    "revision": "51ee9ad5832da4ed41f8",
+    "url": "/Sprint-Challenge-React-Wars/static/js/main.04acbf48.chunk.js"
+  },
+  {
+    "revision": "782454cc9036469b90c9",
+    "url": "/Sprint-Challenge-React-Wars/static/js/runtime-main.3ea5a01b.js"
+  },
+  {
+    "revision": "dff75a1ffda792b2380226033f28415c",
+    "url": "/Sprint-Challenge-React-Wars/static/media/sw-bg.dff75a1f.jpg"
+  }
+]);
